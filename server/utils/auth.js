e2c853235a209a4572d1b732b3c74fa34dae04e0
Eric Smyth <somegoodmilk@gmail.com>
2023-10-28 02:51:25 -0400

    restarted, new inital commit

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,30 +1,39 @@
 const jwt = require('jsonwebtoken');
 
-// Set token secret and expiration date
+// set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // Function for our authenticated routes
-  authMiddleware: function ({ req }) { // Destructure 'req' from context object
-    // Get the token from the 'req' object
-    const token = req.headers.authorization ? req.headers.authorization.split(' ').pop().trim() : null;
+  // function for our authenticated routes
+  authMiddleware: function (req, res, next) {
+    // allows token to be sent via  req.query or headers
+    let token = req.query.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
 
     if (!token) {
-      throw new Error('You have no token!');
+      return res.status(400).json({ message: 'You have no token!' });
     }
 
+    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      // Add user data to the context object
-      return { user: data };
-    } catch (err) {
+      req.user = data;
+    } catch {
       console.log('Invalid token');
-      throw new Error('Invalid token!');
+      return res.status(400).json({ message: 'invalid token!' });
     }
+
+    // send to next endpoint
+    next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
+
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
